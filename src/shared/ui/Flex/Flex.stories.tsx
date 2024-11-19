42ea4a442cf93cbc145b51a1c7ffff0f11bb5dde
Angry Dev <angrybear0911@gmail.com>
2024-11-20 00:44:35 +0900

    Fixed the code style issue by using prettier configuration

--- src/shared/ui/Flex/Flex.stories.tsx
+++ src/shared/ui/Flex/Flex.stories.tsx
@@ -1,52 +1,130 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Flex from './Flex'
+import Flex from "./Flex";
+import { FlexAlign, FlexDirection, FlexGap, FlexJustify } from "./lib/types";
 
 const meta: Meta<typeof Flex> = {
-  title: 'DESIGN_SYSTEM: Flex',
+  title: "DESIGN_SYSTEM: Flex",
   component: Flex,
-}
+};
 
-export default meta
+export default meta;
 
 const style: React.CSSProperties = {
-  width: '100%',
-  minHeight: '512px',
-  padding: '56px 24px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  overflow: 'auto',
-}
+  width: "100%",
+  minHeight: "512px",
+  padding: "56px 24px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  overflow: "auto",
+};
 
-const colors = ['#FFAA79', '#EC7373', '#CD9854', '#486D6C', '#BA84FF', '#8489FF']
+const colors = [
+  "#FFAA79",
+  "#EC7373",
+  "#CD9854",
+  "#486D6C",
+  "#BA84FF",
+  "#8489FF",
+];
 
 export const Default: StoryObj<typeof Flex> = {
   args: {
-    align: 'stretch',
-    justify: 'start',
-    direction: 'row',
+    align: "stretch",
+    justify: "start",
+    direction: "row",
     wrap: true,
   },
 
   argTypes: {
     align: {
-      control: 'select',
+      control: "select",
+      options: ["start", "end", "center", "baseline", "stretch"] as FlexAlign[],
     },
     justify: {
-      control: 'select',
+      control: "select",
+      options: [
+        "start",
+        "end",
+        "center",
+        "space-between",
+        "space-around",
+        "space-evenly",
+        "stretch",
+      ] as FlexJustify[],
     },
     direction: {
-      control: 'select',
+      control: "select",
+      options: [
+        "row",
+        "column",
+        "row-reverse",
+        "column-reverse",
+      ] as FlexDirection[],
     },
     gap: {
-      control: 'select',
+      control: "select",
+      options: [
+        "0-5",
+        "0",
+        "1",
+        "1-5",
+        "2",
+        "2-5",
+        "3",
+        "3-5",
+        "4",
+        "4-5",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+      ] as FlexGap[],
     },
     rowGap: {
-      control: 'select',
+      control: "select",
+      options: [
+        "0-5",
+        "0",
+        "1",
+        "1-5",
+        "2",
+        "2-5",
+        "3",
+        "3-5",
+        "4",
+        "4-5",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+      ] as FlexGap[],
     },
     colGap: {
-      control: 'select',
+      control: "select",
+      options: [
+        "0-5",
+        "0",
+        "1",
+        "1-5",
+        "2",
+        "2-5",
+        "3",
+        "3-5",
+        "4",
+        "4-5",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+      ] as FlexGap[],
     },
     inline: {
       table: {
@@ -75,16 +153,16 @@ export const Default: StoryObj<typeof Flex> = {
                 key={index}
                 style={
                   {
-                    width: '128px',
-                    height: '128px',
+                    width: "128px",
+                    height: "128px",
                     backgroundColor: colors[index % colors.length],
                   } as React.CSSProperties
                 }
               />
-            )
+            );
           })}
         </Flex>
       </div>
-    )
+    );
   },
-}
+};
